81966b46b978f80688815e06fd885d74d65b1662
shreevedpathak04 <shreenidhivedpathak120@gmail.com>
2024-06-13 22:53:12 +0530

    predict pileline

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 from src.exception import CustomException
 from sklearn.model_selection import GridSearchCV
-
+import pickle
 
 def save_object(file_path, obj):
     try:
@@ -41,3 +41,11 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     except Exception as e:
         raise CustomException(e, sys)
     
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
